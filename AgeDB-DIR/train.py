89a648da977a707c29a53aca90d2085e91359557
ruizhipu <18651885620@163.com>
2024-10-29 03:09:23 -0400

    debug

--- AgeDB-DIR/train.py
+++ AgeDB-DIR/train.py
@@ -149,7 +149,7 @@ def train_one_epoch(args, model, train_loader,  mi_estimator, opts):
         var, nll_loss = beta_nll_loss(y_pred, y, feature_mi)
         variance_loss = F.mse_loss(var_pred, var)
         loss = nll_loss + variance_loss
-        print(' mi ', feature_mi)
+        print(' mi ', feature_mi[:10])
         print(' nll ', nll_loss)
         print(' var ', variance_loss.dtype)
         print('total loss', loss.dtype)
