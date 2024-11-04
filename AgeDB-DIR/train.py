f24e58102f4747c4f194ef77240f8377af16320e
ruizhipu <18651885620@163.com>
2024-11-04 05:21:12 -0500

    cvpr25: to do

--- AgeDB-DIR/train.py
+++ AgeDB-DIR/train.py
@@ -150,7 +150,7 @@ def train_one_epoch(args, model, train_loader,  mi_estimator, opts):
         opt_mi.step()
         #
         feature_mi = mi_estimator(z)
-        var, nll_loss = beta_nll_loss(y_pred, y, feature_mi)
+        var, nll_loss = beta_nll_loss(y_pred, y, feature_mi, 0)
         variance_loss = F.mse_loss(var_pred, var.to(torch.float32))
         loss = nll_loss + variance_loss
         loss = loss.to(torch.float)
