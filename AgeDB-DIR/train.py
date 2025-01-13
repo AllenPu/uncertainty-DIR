4ae42a0dc442988a914ebada528d904a77d5de03
AllenPu <18651885620@163.com>
2025-01-13 15:12:13 -0500

    add \n

--- AgeDB-DIR/train.py
+++ AgeDB-DIR/train.py
@@ -267,7 +267,7 @@ if __name__ == '__main__':
         #
         with open(output_file, "a+") as file:
             file.write("Epoch "+str(e)+" ")
-            file.write(" ".join(results))
+            file.write(" ".join(results) + '\n')
             file.close()
         #
         
