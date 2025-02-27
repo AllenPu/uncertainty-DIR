83622cea9505688512006d5f38179cb9b11b9d0b
ruizhipu <18651885620@163.com>
2025-02-26 19:42:16 -0500

    change the train and val to have weights

--- AgeDB-DIR/conform.py
+++ AgeDB-DIR/conform.py
@@ -7,6 +7,7 @@ import torch
 def abs_err(model, cal_batch, tau):
     device = next(model.parameters()).device
     with torch.no_grad():
+        print(cal_batch)
         idx, (x, _, w) = cal_batch
         #for idx, (x, _, w) in enumerate(loader):
         x = x.to(device)
