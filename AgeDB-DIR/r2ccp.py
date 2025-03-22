9514ae77749b26f474e4a1e216f3b954a21ed53c
ruizhipu <18651885620@163.com>
2025-03-22 02:11:36 -0400

    add r2ccp

--- AgeDB-DIR/r2ccp.py
+++ AgeDB-DIR/r2ccp.py
@@ -1,5 +1,13 @@
 import torch
 
+
+#
+# return the precentile value from the vector
+#
+def percentile_excluding_index(vector, percentile):
+        percentile_value = torch.quantile(vector, percentile)
+        
+        return percentile_value
 #
 # input is x and model itself
 #
@@ -28,3 +36,50 @@ def get_all_scores(range_vals, x, y, model):
     all_scores = scores[torch.arange(len(x)), indices_up.long()] * weight_up + scores[torch.arange(len(x)), indices_down.long()] * weight_down
     all_scores[bad_indices] = 0
     return scores, all_scores
+
+
+#
+#
+#
+def find_intervals_above_value_with_interpolation(x_values, y_values, cutoff):
+    intervals = []
+    start_x = None
+    if y_values[0] >= cutoff:
+        start_x = x_values[0]
+    for i in range(len(x_values) - 1):
+        x1, x2 = x_values[i], x_values[i + 1]
+        y1, y2 = y_values[i], y_values[i + 1]
+
+        if min(y1, y2) <= cutoff < max(y1, y2):
+            # Calculate the x-coordinate where the line crosses the cutoff value
+            x_cross = x1 + (x2 - x1) * (cutoff - y1) / (y2 - y1)
+
+            if x1 <= x_cross <= x2:
+                if start_x is None:
+                    start_x = x_cross
+                else:
+                    intervals.append((start_x, x_cross))
+                    start_x = None
+
+    # If the line ends above cutoff, add the last interval
+    if start_x is not None:
+        intervals.append((start_x, x_values[-1]))
+
+    return intervals
+
+
+#
+# X is from the train and later is from val
+#
+def get_cp_lists(X, args, range_vals, X_cal, Y_cal, model):
+    scores, all_scores = get_all_scores(range_vals, X_cal, Y_cal, model)
+    pred_scores = get_scores(X, model)
+    alpha = args.alpha
+
+    percentile_val = percentile_excluding_index(all_scores, alpha)
+        
+    all_intervals = []
+    for i in range(len(pred_scores)):
+        all_intervals.append(find_intervals_above_value_with_interpolation(range_vals, pred_scores[i], percentile_val))
+
+    return all_intervals
