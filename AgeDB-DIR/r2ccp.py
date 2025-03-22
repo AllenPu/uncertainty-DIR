e586c907e7c792fc61875bbe698ebf9e9e1c9edc
ruizhipu <18651885620@163.com>
2025-03-22 01:11:17 -0400

    add r2ccp

---
+++ AgeDB-DIR/r2ccp.py
@@ -0,0 +1,27 @@
+import torch
+
+
+
+def get_scores(X, model):
+    scores = torch.nn.functional.softmax(model(torch.tensor(X, dtype=torch.float32)), dim=1)
+    return scores
+
+
+def get_all_scores(range_vals, X, y, model):
+    step_val = (max(range_vals) - min(range_vals))/(len(range_vals) - 1)
+    indices_up = torch.ceil((y - min(range_vals))/step_val).squeeze()
+    indices_down = torch.floor((y - min(range_vals))/step_val).squeeze()
+    
+    how_much_each_direction = ((y.squeeze() - min(range_vals))/step_val - indices_down)
+
+    weight_up = how_much_each_direction
+    weight_down = 1 - how_much_each_direction
+
+    bad_indices = torch.where(torch.logical_or(y.squeeze() > max(range_vals), y.squeeze() < min(range_vals)))
+    indices_up[bad_indices] = 0
+    indices_down[bad_indices] = 0
+    
+    scores = get_scores(X, model)
+    all_scores = scores[torch.arange(len(X)), indices_up.long()] * weight_up + scores[torch.arange(len(X)), indices_down.long()] * weight_down
+    all_scores[bad_indices] = 0
+    return scores, all_scores
