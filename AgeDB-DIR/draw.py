775ff5be2ee103e0504a468d4ea1fd8df8d2e19c
AllenPu <18651885620@163.com>
2025-04-03 20:37:40 -0400

    add draw files

---
+++ AgeDB-DIR/draw.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+#x = np.arange(0., np.e, 0.01)
+#y1 = np.exp(-x)
+#y2 = np.log(x)
+
+f = 'C:\Users\rpu2\Desktop\revised NLL MSE MAE + LDS.csv'
+df = pd.read_csv(f)
+
+
+for name in ['NLL', 'NLL_LDS', 'MSE', 'MSE_LDS', 'MAE', 'MAE_LDS']: 
+    x = df['Train label'].values
+    y1 = df[f'{name} Train Frob'].values
+    y2 = df[f'{name} Train MAE'].values
+    y3 = df[f'{name} Test Frob'].values
+    y4 = df[f'{name} Test MAE'].values
+
+
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111)
+    ax1.plot(x, y1,'r',label="Train Frob")
+    ax1.plot(x, y3,'g', label="Train MAE")
+    ax1.legend(loc=1)
+    ax1.set_ylabel('Frob')#
+    ax2 = ax1.twinx() # this is the important function
+    ax2.plot(x, y2, 'y',label = "Test Frob")
+    ax2.plot(x, y4, 'pink',label = "Test MAE")
+    ax2.legend(loc=2)
+    ax2.set_xlim([0, 100])
+    ax2.set_ylabel('MAE')
+    ax2.set_xlabel('Train Label')
+    plt.savefig(f'./{name}.jpg')
