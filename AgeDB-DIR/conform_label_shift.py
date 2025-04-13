dc447d4b008c429b7993dbc9f90a59b7f6794860
ruizhipu <18651885620@163.com>
2025-04-12 22:55:55 -0400

    label shift conformal

--- AgeDB-DIR/conform_label_shift.py
+++ AgeDB-DIR/conform_label_shift.py
@@ -16,7 +16,7 @@ def lscp_quantile(weights, calib_preds, calib_targets, alpha=0.1):
     sorted_weights = cal_weights[sorted_idx]
     cum_weights = torch.cumsum(sorted_weights, dim=0)
     cutoff = (1 - alpha) * cum_weights[-1]
-    print('~~~~~~~idx is~~~~~~', idx)
+    print('~~~~~~~idx is~~~~~~', cutoff)
     idx = torch.searchsorted(cum_weights, cutoff)
     print('~~~~~~~idx shape is~~~~~~', idx.shape)
     q = sorted_residuals[min(idx, len(sorted_residuals)-1)]
