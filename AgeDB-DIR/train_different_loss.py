4c4c28d52f205d477f9c6fa4206fe2d6c162ecb0
AllenPu <18651885620@163.com>
2025-07-21 18:41:12 -0400

    debug

--- AgeDB-DIR/train_different_loss.py
+++ AgeDB-DIR/train_different_loss.py
@@ -318,7 +318,7 @@ def train_with_nll(y, y_pred, y_lower, y_upper, cal_batch, e):
         # train with MSE
         var_pred = torch.ones(y.shape).to(device)
     beta = int(args.beta)
-    print(f'==================== {type(args.beta)}')
+    #print(f'==================== {type(args.beta)}')
     #print(f' interval shape {interval.shape} y shape {y.shape}')
     nll = torch.mean(beta_nll_loss(y_pred, var_pred, y, beta=beta, e = e))
     nll += addtion_loss
